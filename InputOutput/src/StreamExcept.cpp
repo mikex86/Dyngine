fe3d1c5f8089f7788566b7cc221680222b2e03e1
Mike <michael.keiblinger@gmail.com>
2021-12-18 15:14:57 +0100

    Big refactor

--- InputOutput/src/StreamExcept.cpp
+++
@@ -1,7 +0,0 @@
-#include <Stream/StreamExcept.hpp>
-
-using namespace Stream;
-
-EXCEPTION_TYPE_DEFAULT_IMPL(StreamSeekException);
-EXCEPTION_TYPE_DEFAULT_IMPL(StreamWriteException);
-EXCEPTION_TYPE_DEFAULT_IMPL(StreamReadException);
--- shaders/triangle/triangle.frag.glsl
+++
@@ -1,9 +0,0 @@
-#version 450 core
-
-layout(location = 0) in vec3 vertexColor;
-layout(location = 0) out vec4 fragColor;
-
-void main()
-{
-    fragColor = vec4(vertexColor, 1);
-}
